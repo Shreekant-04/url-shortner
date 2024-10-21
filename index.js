5fb60baf5b1ab9266f8843267d22c5c6b34cace6
Shreekant <shree.kant.74.sk@gmail.com>
2024-10-22 01:19:52 +0530

    another

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const connection = require("./config/db");
 const urlSchema = require("./models/urlSchema");
-const genShortUrl = require("./services/generateurl");
-const { URL } = require("url");
 var path = require("path");
 
 require("dotenv").config();
@@ -18,6 +16,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
+// Helper function to validate URL
 const validateUrl = (urlString) => {
   try {
     const url = new URL(urlString);
@@ -27,48 +26,55 @@ const validateUrl = (urlString) => {
   }
 };
 
-app.get("/", (req, res) => {
+// Root endpoint to render the home page
+app.get("/", async (req, res) => {
   res.set("Content-Type", "text/html");
   res.render("index", { active: "result", LongUrl: "", shortUrl: "" });
 });
 
+// Shorten URL endpoint
 app.post("/api/shorturl", async (req, res) => {
   const { url } = req.body;
 
+  // Check if the URL is valid
   if (!validateUrl(url)) {
     return res.json({ error: "invalid url" });
   }
 
+  // Check if the URL is already shortened
   let data = await urlSchema.findOne({ LongUrl: url });
   if (data) {
     return res
       .status(200)
       .json({ original_url: data.LongUrl, short_url: data.shortUrl });
   }
+  const totalDocuments = await urlSchema.countDocuments();
+  const shortUrl = totalDocuments + 1;
 
-  const shortUrl = genShortUrl(url);
+  // Create and save the new URL entry
   let newEntry = new urlSchema({ LongUrl: url, shortUrl });
-
-  newEntry.shortUrl = `https://${req.headers.host}/api/shorturl/${shortUrl}`;
   await newEntry.save();
 
-  res
-    .status(201)
-    .json({ original_url: newEntry.LongUrl, short_url: newEntry.shortUrl });
+  // Return the original URL and the shortened URL identifier
+  res.status(201).json({ original_url: newEntry.LongUrl, short_url: shortUrl });
 });
 
+// Redirect from short URL to original URL
 app.get("/api/shorturl/:shorturl", async (req, res) => {
-  const shortUrl = `https://${req.headers.host}/api/shorturl/${req.params.shorturl}`;
+  const { shorturl } = req.params;
 
-  const data = await urlSchema.findOne({ shortUrl });
+  // Find the corresponding long URL in the database
+  const data = await urlSchema.findOne({ shortUrl: shorturl });
   if (data) {
     return res.redirect(data.LongUrl);
   }
 
+  // If the short URL does not exist, return a 404 response
   res.set("Content-Type", "text/html");
-  res.render("notFound");
+  res.status(404).render("notFound");
 });
 
+// Start the server
 let PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
